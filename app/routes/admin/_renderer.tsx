65527c6215d9e8ddb67109373e0ce4155d95dc7c
paspulsa <paspulsa.com@gmail.com>
2026-05-27 01:08:16 +0700

    Update _renderer.tsx

--- app/routes/admin/_renderer.tsx
+++ app/routes/admin/_renderer.tsx
@@ -50,7 +50,7 @@ export default jsxRenderer(({ children }) => {
         
         {/* Tombol Keluar / Logout */}
         <div class="p-4 border-t border-slate-800 bg-slate-950">
-          <a href="/auth/logout" class="flex items-center justify-center gap-2 w-full py-2.5 px-4 rounded-lg font-bold text-sm bg-red-600 hover:bg-red-700 text-white transition-colors shadow-md">
+          <a href="/admin/logout" class="flex items-center justify-center gap-2 w-full py-2.5 px-4 rounded-lg font-bold text-sm bg-red-600 hover:bg-red-700 text-white transition-colors shadow-md">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
             Keluar Aplikasi
           </a>
