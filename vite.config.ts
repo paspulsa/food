3b3cb716d6ac36ad49ae680ddc4418db3efbc7e7
paspulsa <paspulsa.com@gmail.com>
2026-05-27 00:17:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import pages from '@hono/vite-cloudflare-pages'
 export default defineConfig({
   plugins: [
     honox({
-      // HonoX hanya akan fokus membaca folder app/
       entry: './app/server.ts', 
     }),
-    pages()
+    pages({
+      entry: './app/server.ts'
+    })
   ]
 })
