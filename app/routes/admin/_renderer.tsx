ec0a17300e5be3e711d2bdb11c43cd682f439f02
paspulsa <paspulsa.com@gmail.com>
2026-05-28 01:10:55 +0700

    Update _renderer.tsx

--- app/routes/admin/_renderer.tsx
+++ app/routes/admin/_renderer.tsx
@@ -88,7 +88,7 @@ export default jsxRenderer(({ children, title }) => {
                   Produk & Stok
                 </div>
               </a>
-              <a href="/admin/promo" class="nav-link flex items-center justify-between px-3 py-2.5 rounded-lg text-gray-600 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-800/50 hover:text-primary dark:hover:text-primary transition-all font-medium text-sm">
+              <a href="/admin/promos" class="nav-link flex items-center justify-between px-3 py-2.5 rounded-lg text-gray-600 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-800/50 hover:text-primary dark:hover:text-primary transition-all font-medium text-sm">
                 <div class="flex items-center gap-3">
                   <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                   Banner Promo
