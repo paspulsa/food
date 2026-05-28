77e062f50e2d40389f333b2573ba8a0522ce6cbc
paspulsa <paspulsa.com@gmail.com>
2026-05-28 09:21:35 +0700

    Update _renderer.tsx

--- app/routes/admin/_renderer.tsx
+++ app/routes/admin/_renderer.tsx
@@ -94,7 +94,7 @@ export default jsxRenderer(({ children, title }) => {
                   Banner Promo
                 </div>
               </a>
-              <a href="/admin/member" class="nav-link flex items-center gap-3 px-3 py-2.5 rounded-lg text-gray-600 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-800/50 hover:text-primary dark:hover:text-primary transition-all font-medium text-sm">
+              <a href="/admin/members" class="nav-link flex items-center gap-3 px-3 py-2.5 rounded-lg text-gray-600 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-800/50 hover:text-primary dark:hover:text-primary transition-all font-medium text-sm">
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                 Pelanggan (CRM)
               </a>
